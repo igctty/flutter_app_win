6f0f025b888c54a3974b23f3766adb38bb86ea8a
igctty <tatsu.smartphone@gmail.com>
2020-10-25 20:22:20 +0900

    Flutter layout demo Step3 WIP.

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,19 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    Color color = Theme.of(context).primaryColor;
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
@@ -14,6 +27,7 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             titleSection,
+            buttonSection,
           ],
         ),
       ),
@@ -25,11 +39,9 @@ class MyApp extends StatelessWidget {
     child: Row(
       children: [
         Expanded(
-          /*1*/
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              /*2*/
               Container(
                 padding: const EdgeInsets.only(bottom: 8),
                 child: Text(
@@ -48,7 +60,6 @@ class MyApp extends StatelessWidget {
             ],
           ),
         ),
-        /*3*/
         Icon(
           Icons.star,
           color: Colors.red[500],
@@ -57,4 +68,28 @@ class MyApp extends StatelessWidget {
       ],
     ),
   );
+
+
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
+
