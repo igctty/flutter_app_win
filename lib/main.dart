c0e1bef8f865a8051ba00595b60a94111a37ff13
igctty <tatsu.smartphone@gmail.com>
2020-10-26 19:56:22 +0900

    Flutter layout A mix-and-match approach done.

--- lib/main.dart
+++ lib/main.dart
@@ -163,7 +163,7 @@ class _FavoriteWidgetState extends State<FavoriteWidget> {
 
 // ParentWidget manages the state for TapboxB.
 
-//------------------------ ParentWidget --------------------------------
+//---------------------------- ParentWidget ----------------------------
 
 class ParentWidget extends StatefulWidget {
   @override
@@ -182,7 +182,7 @@ class _ParentWidgetState extends State<ParentWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TapboxB(
+      child: TapboxC(
         active: _active,
         onChanged: _handleTapboxChanged,
       ),
@@ -190,33 +190,67 @@ class _ParentWidgetState extends State<ParentWidget> {
   }
 }
 
-//------------------------- TapboxB ----------------------------------
+//----------------------------- TapboxC ------------------------------
 
-class TapboxB extends StatelessWidget {
-  TapboxB({Key key, this.active: false, @required this.onChanged})
+class TapboxC extends StatefulWidget {
+  TapboxC({Key key, this.active: false, @required this.onChanged})
       : super(key: key);
 
   final bool active;
   final ValueChanged<bool> onChanged;
 
+  _TapboxCState createState() => _TapboxCState();
+}
+
+class _TapboxCState extends State<TapboxC> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
   void _handleTap() {
-    onChanged(!active);
+    widget.onChanged(!widget.active);
   }
 
   Widget build(BuildContext context) {
+    // This example adds a green border on tap down.
+    // On tap up, the square changes to the opposite state.
     return GestureDetector(
+      onTapDown: _handleTapDown, // Handle the tap events in the order that
+      onTapUp: _handleTapUp, // they occur: down, up, tap, cancel
       onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
       child: Container(
         child: Center(
-          child: Text(
-            active ? 'Active' : 'Inactive',
-            style: TextStyle(fontSize: 32.0, color: Colors.white),
-          ),
+          child: Text(widget.active ? 'Active' : 'Inactive',
+              style: TextStyle(fontSize: 32.0, color: Colors.white)),
         ),
         width: 200.0,
         height: 200.0,
         decoration: BoxDecoration(
-          color: active ? Colors.lightGreen[700] : Colors.grey[600],
+          color:
+          widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _highlight
+              ? Border.all(
+            color: Colors.teal[700],
+            width: 10.0,
+          )
+              : null,
         ),
       ),
     );
