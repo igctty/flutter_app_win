e185867d38d632331eb654732a5d3ba407e3fbc0
igctty <tatsu.smartphone@gmail.com>
2020-10-28 21:37:47 +0900

    Icon sample done.

--- lib/main.dart
+++ lib/main.dart
@@ -1,136 +1,75 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    BooksApp()
-  );
+  runApp(MyApp());
 }
 
-class Book {
-  final String title;
-  final String author;
-
-  Book(this.title, this.author);
-}
-
-class BooksApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _BooksAppState();
-}
-
-class _BooksAppState extends State<BooksApp> {
-  void initState() {
-    super.initState();
-  }
-
-  Book _selectedBook;
-  bool show404 = false;
-  List<Book> books = [
-    Book('hogehoge', 'HogeHoge'),
-    Book('fugafuga', 'FugaFuga'),
-    Book('special book', 'igctty'),
-  ];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Books App',
-      home: Navigator(
-        pages: [
-          MaterialPage(
-            key: ValueKey('BooksListPage'),
-            child: BooksListScreen(
-              books: books,
-              onTapped: _handleBookTapped,
-            ),
-          ),
-          if (_selectedBook != null) BookDetailsPage(book: _selectedBook),
-        ],
-        onPopPage: (route, result) {
-          if (!route.didPop(result)) {
-            return false;
-          }
-          setState(() {
-            _selectedBook = null;
-          });
-
-          return true;
-        },
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: MyHomePage(title: 'Fluppy'),
     );
   }
-  void _handleBookTapped(Book book) {
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
     setState(() {
-      _selectedBook = book;
+      _counter++;
     });
   }
-}
-
-class BooksListScreen extends StatelessWidget {
-  final List<Book> books;
-  final ValueChanged<Book> onTapped;
-  BooksListScreen({
-    @required this.books,
-    @required this.onTapped,
-  });
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: ListView(
-        children: [
-          for (var book in books)
-            ListTile(
-              title: Text(book.title),
-              subtitle: Text(book.author),
-              onTap: () => onTapped(book),
-            )
-        ],
-      ),
-    );
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
   }
-}
-
-class BookDetailsScreen extends StatelessWidget {
-  final Book book;
-
-  BookDetailsScreen({
-    @required this.book,
-  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (book != null) ...[
-              Text(book.title, style: Theme.of(context).textTheme.headline6),
-              Text(book.author, style: Theme.of(context).textTheme.subtitle1),
-            ],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            IconButton(
+              icon: Icon(Icons.add),
+              tooltip: 'Increment',
+              onPressed: _incrementCounter,
+            ),
+            IconButton(
+              icon: Icon(Icons.remove),
+              tooltip: 'Decrement',
+              onPressed: _decrementCounter,
+            )
           ],
         ),
       ),
-    );
-  }
-}
-
-class BookDetailsPage extends Page {
-  final Book book;
-
-  BookDetailsPage({
-    this.book,
-  }) : super(key: ValueKey(book));
-
-  Route createRoute(BuildContext context) {
-    return MaterialPageRoute(
-      settings: this,
-      builder: (BuildContext context) {
-        return BookDetailsScreen(book: book);
-      },
     );
   }
 }
