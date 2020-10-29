172f20c81ef3026b53c8db2bd9385390e8695e08
igctty <tatsu.smartphone@gmail.com>
2020-10-29 20:04:09 +0900

    TODO app initial code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/animation.dart';
 
-void main() => runApp(LogoApp());
+void main() => runApp(MyApp());
 
-class LogoApp extends StatefulWidget {
-  _LogoAppState createState() => _LogoAppState();
-}
-
-class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
-  Animation<double> animation;
-  AnimationController controller;
-
+class MyApp extends StatelessWidget {
+  static const String _title = 'TODO List';
   @override
-  void initState() {
-    super.initState();
-    controller = AnimationController(
-      duration: const Duration(seconds: 2),
-      vsync: this
-    );
-    animation = Tween<double>(begin: 0, end: 300).animate(controller)
-    ..addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        controller.reverse();
-      } else if (status == AnimationStatus.dismissed) {
-        controller.forward();
-      }
-    });
-    controller.forward();
-  }
-
-  @override
-  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-}
-class AnimatedLogo extends AnimatedWidget {
-  AnimatedLogo({Key key, Animation<double> animation})
-      : super(key: key, listenable: animation);
-
   Widget build(BuildContext context) {
-    final animation = listenable as Animation<double>;
-    return Center(
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 10),
-        height: animation.value,
-        width: animation.value,
-        child: FlutterLogo(),
-      ),
+    return MaterialApp(
+      title: _title,
+      home: Text("List item 1"),
     );
   }
 }
+
