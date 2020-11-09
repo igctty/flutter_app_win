508e4b656b1b6fcb5256b5024981ef898badf180
igctty <tatsu.smartphone@gmail.com>
2020-11-09 20:45:33 +0900

    chore: TODO deleted.

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,6 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       appBar: AppBar(
         title: const Text('ホーム'),
       ),
-      // TODO: 詳細ページ遷移時にアイテムの内容を引き継ぐようにする
       // TODO: TODOアイテムの横幅と削除アイコンの横幅を画面内に収める
       body: CustomScrollView(
         center: centerKey,
