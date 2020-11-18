6ab19ceb9d560b7fd322643e377e718c77f80755
igctty <tatsu.smartphone@gmail.com>
2020-11-18 18:16:22 +0900

    The indicator area added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 const kColorPurple = Color(0xFF8337EC);
 const kColorPink = Color(0xFFFF006F);
@@ -44,6 +45,7 @@ class MainPage extends StatelessWidget {
         child: Column(
           children: [
             CategoriesButtons(),
+            Indicator(),
           ],
         ),
       ),
@@ -95,6 +97,82 @@ class CategoriesButtons extends StatelessWidget {
             _CategoriesButton(text: 'Category fugafuga'),
             SizedBox(width: 16),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class _IndicatorPainter extends CustomPainter {
+  final double percentage; // 表示された値の割合
+  final double textCircleRadius; // 内側に表示される白丸の半径
+  _IndicatorPainter({
+    @required this.percentage,
+    @required this.textCircleRadius,
+  });
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (int i = 1; i < (360 * percentage); i += 5) {
+      final per = i / 360.0;
+      // 割合（0~1.0）からグラデーション色に変換
+      final color = ColorTween(
+        begin: kColorIndicatorBegin,
+        end: kColorIndicatorEnd,
+      ).lerp(per);
+      final paint = Paint()
+        ..color = color
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 4;
+      final spaceLen = 16; // 円とゲージ間の長さ
+      final lineLen = 24; // ゲージの長さ
+      final angle = (2 * pi * per) - (pi / 2); // 0時方向から開始するため-90度ずらす
+      // 円の中心座標
+      final offset0 = Offset(size.width * 0.5, size.height * 0.5);
+      // 線の内側部分の座標
+      final offset1 = offset0.translate(
+        (textCircleRadius + spaceLen) * cos(angle),
+        (textCircleRadius + spaceLen) * sin(angle),
+      );
+      // 線の外側部分の座標
+      final offset2 = offset1.translate(
+        lineLen * cos(angle),
+        lineLen * sin(angle),
+      );
+      canvas.drawLine(offset1, offset2, paint);
+    }
+  }
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+class Indicator extends StatelessWidget {
+  final percentage = 0.7;
+  final size = 164.0;
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: _IndicatorPainter(
+        percentage: percentage,
+        textCircleRadius: size * 0.5,
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(64),
+        child: Material(
+          color: Colors.white,
+          elevation: kElevation,
+          borderRadius: BorderRadius.circular(size * 0.5),
+          child: Container(
+            width: size,
+            height: size,
+            child: Center(
+              child: Text(
+                '${percentage * 100}%',
+                style: TextStyle(color: kColorPink, fontSize: 48),
+              ),
+            ),
+          ),
         ),
       ),
     );
