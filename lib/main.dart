24eb105fd765af1c62938f8eb8a2244dc0863b30
igctty <tatsu.smartphone@gmail.com>
2020-11-19 21:23:46 +0900

    The Applist area & optimize button added.

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,8 @@ class MainPage extends StatelessWidget {
           children: [
             CategoriesButtons(),
             Indicator(),
+            AppsDrainage(),
+            OptimizeButton(),
           ],
         ),
       ),
@@ -174,6 +176,123 @@ class Indicator extends StatelessWidget {
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _HorizontalBorder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 1,
+      margin: EdgeInsets.symmetric(horizontal: 16),
+      color: Colors.grey[200],
+    );
+  }
+}
+class _AppColumn extends StatelessWidget {
+  final String name;
+  final Icon icon;
+  final String percentage;
+  const _AppColumn({
+    Key key,
+    @required this.name,
+    @required this.icon,
+    @required this.percentage,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: icon,
+      title: Text(
+        name,
+        style: TextStyle(color: kColorText),
+      ),
+      trailing: Text(
+        percentage,
+        style: TextStyle(color: kColorText),
+      ),
+    );
+  }
+}
+class AppsDrainage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          leading: ClipOval(
+            child: Container(
+              color: kColorPurple,
+              child: Icon(Icons.apps, color: Colors.white),
+            ),
+          ),
+          title: Text(
+            'Apps Drainage',
+            style: TextStyle(color: kColorTitle),
+          ),
+          subtitle: Text(
+            'Show the most draining energy application',
+            style: TextStyle(color: kColorText),
+          ),
+        ),
+        Material(
+          color: Colors.white,
+          elevation: kElevation,
+          borderRadius: BorderRadius.circular(16),
+          child: Column(
+            children: [
+              _AppColumn(
+                icon: Icon(Icons.sms, color: Colors.indigo),
+                name: 'SMSApp',
+                percentage: '75%',
+              ),
+              _HorizontalBorder(),
+              _AppColumn(
+                icon: Icon(Icons.live_tv, color: Colors.red),
+                name: 'MovieApp',
+                percentage: '60%',
+              ),
+              _HorizontalBorder(),
+              _AppColumn(
+                icon: Icon(Icons.place, color: Colors.green),
+                name: 'MapApp',
+                percentage: '35%',
+              ),
+              _HorizontalBorder(),
+              _AppColumn(
+                icon: Icon(Icons.local_grocery_store, color: Colors.orange),
+                name: 'ShoppingApp',
+                percentage: '35%',
+              ),
+              _HorizontalBorder(),
+              _AppColumn(
+                icon: Icon(Icons.train, color: Colors.black),
+                name: 'TrainApp',
+                percentage: '15%',
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class OptimizeButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 24),
+      child: RaisedButton(
+        color: kColorPurple,
+        padding: EdgeInsets.symmetric(horizontal: 48),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        onPressed: () {},
+        child: Text('Optimize', style: TextStyle(color: Colors.white)),
       ),
     );
   }
