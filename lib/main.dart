b67258fe641d0b68c137f61f89ff5ab22a41bc76
igctty <tatsu.smartphone@gmail.com>
2020-11-22 12:53:15 +0900

    UI_Template2 added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,276 @@
 import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
-
+void main() {
+  runApp(MyApp());
+}
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final appTitle = 'Form Validation Demo';
-
     return MaterialApp(
-      title: appTitle,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(appTitle),
-        ),
-        body: MyCustomForm(),
+      debugShowCheckedModeBanner: false,
+      title: 'UI_Template2',
+      theme: ThemeData.light(),
+      home: AdminMobilePage(),
+    );
+  }
+}
+class AdminMobilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          SideNavigation(),
+          VerticalDivider(thickness: 1, width: 1),
+          Expanded(
+            child: PostList(),
+          ),
+        ],
       ),
     );
   }
 }
 
-// Create a Form widget.
-class MyCustomForm extends StatefulWidget {
+class SideNavigation extends StatefulWidget {
   @override
-  MyCustomFormState createState() {
-    return MyCustomFormState();
+  _SideNavigationState createState() => _SideNavigationState();
+}
+class _SideNavigationState extends State<SideNavigation> {
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return NavigationRail(
+      selectedIndex: selectedIndex,
+      onDestinationSelected: (index) {
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      destinations: [
+        NavigationRailDestination(
+          icon: Icon(Icons.thumbs_up_down),
+          label: Text('ThumbsUpDown'),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.people),
+          label: Text('People'),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.face),
+          label: Text('Face'),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.bookmark),
+          label: Text('Bookmark'),
+        ),
+      ],
+    );
   }
 }
 
-class MyCustomFormState extends State<MyCustomForm> {
-  final _formKey = GlobalKey<FormState>();
 
+class _PostsHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: ListTile(
+            leading: ClipOval(
+              child: Container(
+                color: Colors.grey[300],
+                width: 48,
+                height: 48,
+                child: Icon(
+                  Icons.storage,
+                  color: Colors.grey[800],
+                ),
+              ),
+            ),
+            title: Text('Posts'),
+            subtitle: Text('20 Posts'),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState.validate()) {
-                  Scaffold.of(context)
-                      .showSnackBar(SnackBar(content: Text('Success')));
-                }
-              },
-              child: Text('Submit'),
+        ),
+        Expanded(
+          flex: 1,
+          child: ListTile(
+            leading: ClipOval(
+              child: Container(
+                color: Colors.grey[300],
+                width: 48,
+                height: 48,
+                child: Icon(
+                  Icons.style,
+                  color: Colors.grey[800],
+                ),
+              ),
+            ),
+            title: Text('All Types'),
+            subtitle: Text(''),
+          ),
+        ),
+      ],
+    );
+  }
+}
+class _Post extends StatelessWidget {
+  final String name;
+  final String message;
+  final String textReason;
+  final Color colorPrimary;
+  final Color colorPositive;
+  final String textPositive;
+  final Color colorNegative;
+  final String textNegative;
+  const _Post({
+    Key key,
+    @required this.name,
+    @required this.message,
+    @required this.textReason,
+    @required this.colorPrimary,
+    @required this.colorPositive,
+    @required this.textPositive,
+    @required this.colorNegative,
+    @required this.textNegative,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      child: Card(
+        elevation: 8,
+        shadowColor: Colors.grey,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          children: [
+            ListTile(
+              leading: ClipOval(
+                child: Container(
+                  color: colorPrimary,
+                  width: 48,
+                  height: 48,
+                  child: Center(
+                    child: Text(
+                      name.substring(0, 1),
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                    ),
+                  ),
+                ),
+              ),
+              title: Text(name),
+              subtitle: Text('2 min ago'),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(width: 72),
+                  Container(
+                    width: 16,
+                    height: 16,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: colorPrimary, width: 4),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  Flexible(child: Text(message)),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              child: Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(color: colorPrimary, width: 2),
+                      ),
+                    ),
+                    child: Text(
+                      textReason,
+                      style: TextStyle(color: Colors.blueAccent),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: FlatButton(
+                      textColor: colorNegative,
+                      onPressed: () {},
+                      child: Text(textNegative),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: FlatButton(
+                      color: colorPositive.withOpacity(0.2),
+                      textColor: colorPositive,
+                      onPressed: () {},
+                      child: Text(textPositive),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+class _PostGreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _Post(
+      name: 'Pean',
+      message: 'Weak reason. No action required.',
+      textReason: 'Report Details',
+      colorPrimary: Colors.greenAccent,
+      colorPositive: Colors.greenAccent,
+      textPositive: 'Keep',
+      colorNegative: Colors.blueAccent,
+      textNegative: 'Archive',
+    );
+  }
+}
+class _PostRed extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _Post(
+      name: 'Orange ',
+      message: 'Not recomended for publication.',
+      textReason: 'Pending Reason',
+      colorPrimary: Colors.deepOrangeAccent,
+      colorPositive: Colors.blueAccent,
+      textPositive: 'Publish',
+      colorNegative: Colors.deepOrangeAccent,
+      textNegative: 'Decline',
+    );
+  }
+}
+class PostList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 48),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          _PostsHeader(),
+          Expanded(
+            child: ListView(
+              children: [
+                _PostGreen(),
+                _PostRed(),
+                _PostGreen(),
+                _PostRed(),
+                _PostGreen(),
+                _PostRed(),
+              ],
             ),
           ),
         ],
